bf5962dd25ec8ff665175a277cf3e7a3666a2318
qulei <qulei_20180331@163.com>
2019-12-03 23:11:02 +0800

    Add:增加游戏服务器项目结构

---
+++ HeroStroy/src/main/java/org/tinygame/herostory/GameMsgHandler.java
@@ -0,0 +1,14 @@
+package org.tinygame.herostory;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author QuLei
+ */
+public class GameMsgHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("收到客户端的消息：" + msg);
+    }
+}
