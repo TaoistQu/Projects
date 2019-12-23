21a85a2e0c8c0ce045826a54e04d49b29dbd99d0
qulei <qulei_20180331@163.com>
2019-12-23 23:50:52 +0800

    Add : 用Dubbo拆分项目结构

---
+++ OA-Dubbo/provider/src/test/java/cn/qulei/oa/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.qulei.oa.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
